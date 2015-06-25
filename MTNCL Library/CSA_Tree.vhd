558bb0a8da0487a3fa201399051ed577648267ef
bab010 <bab010@uark.edu>
2015-06-25 12:33:50 -0500

    Finished - Untested CSA Tree

--- MTNCL Library/CSA_Tree.vhd
+++ MTNCL Library/CSA_Tree.vhd
@@ -24,8 +24,11 @@ architecture arch of CSA_Tree is
 	
 	type XarrayType is array(natural range <>) of dual_rail_logic_vector(15 downto 0);
 	
-	signal L0 : XarrayType(11 downto 0);
+	signal L0 : XarrayType(10 downto 0);
 	signal L1 : XarrayType(7 downto 0);
+	signal L2 : XarrayType(5 downto 0);
+	signal L3 : XarrayType(3 downto 0);
+	signal L4 : XarrayType(2 downto 0);
 
 begin
 	L0Gen: for i in 0 to 4 generate
@@ -33,13 +36,37 @@ begin
 			generic map(16)
 			port map(X(3 * i), X(3 * i + 1), X(3 * i + 2), sleep, L0(2 * i), L0(2 * i + 1));
 	end generate;
-	L0(10) <= X(14);
-	L0(11) <= X(15);
+	L0(10) <= X(15);
 	
-	L1Gen: for i in 0 to 3 generate
+	L1Gen: for i in 0 to 2 generate
 		CSA1 : CSA_genm
 			generic map(16)
 			port map(L0(3 * i), L0(3 * i + 1), L0(3 * i + 2), sleep, L1(2 * i), L1(2 * i + 1));
+	end generate;
+	L1(6) <= L0(9);
+	L1(7) <= L0(10);
 	
+	L2Gen: for i in 0 to 1 generate
+		CSA2 : CSA_genm
+			generic map(16)
+			port map(L1(3 * i), L1(3 * i + 1), L1(3 * i + 2), sleep, L2(2 * i), L2(2 * i + 1));
+	end generate;
+	L2(4) <= L1(6);
+	L2(5) <= L1(7);
+	
+	L3Gen: for i in 0 to 1 generate
+		CSA3 : CSA_genm
+			generic map(16)
+			port map(L2(3 * i), L2(3 * i + 1), L2(3 * i + 2), sleep, L3(2 * i), L3(2 * i + 1));
+	end generate;
+	
+	CSA4 : CSA_genm
+		generic map(16)
+		port map(L3(0), L3(1), L3(2), sleep, L4(0), L4(1));
+	L4(2) <= L3(3);
+	
+	CSA5 : CSA_genm
+		generic map(16)
+		port map(L4(0), L4(1), L4(2), sleep, COUT, S);
 
 end arch;
--- Testbenches/tb_FIR_Timing.vhd
+++ Testbenches/tb_FIR_Timing.vhd
@@ -17,7 +17,7 @@ architecture arch of tb_FIR_Timing_Unpipelined is
 	signal Y                            : DUAL_RAIL_LOGIC_VECTOR(10 downto 0);
 	signal sleep, ki, ko, sleepout, rst : std_logic;
 
-	component FIR_BaughWooley_RCA_Unpipelined is
+	component FIR_Partially_Merged_RCA_Unpipelined is
 		port(X        : in  dual_rail_logic_vector(9 downto 0);
 			 C        : in  CType;
 			 ki       : in  std_logic;
@@ -29,7 +29,7 @@ architecture arch of tb_FIR_Timing_Unpipelined is
 	end component;
 
 begin
-	CUT : FIR_BaughWooley_RCA_Unpipelined
+	CUT : FIR_Partially_Merged_RCA_Unpipelined
 		port map(X, C, ki, rst, sleep, ko, sleepout, Y);
 
 	inputs : process

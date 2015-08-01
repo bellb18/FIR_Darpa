d2395e541e9acfa1555c4a1dda32ec9604788bf6
bab010 <bab010@uark.edu>
2015-07-31 21:58:03 -0500

    Fixed Dadda 5

--- FIR_Architectures/FIR_Liang_Pipelined_Dadda5.vhd
+++ FIR_Architectures/FIR_Liang_Pipelined_Dadda5.vhd
@@ -26,7 +26,7 @@ architecture arch of FIR_Liang_Pipelined_Dadda5 is
 		 p     : out dual_rail_logic_vector(15 downto 0));
 	end component;
 
-	component Carry_Select_16bm is
+	component RCA_Pipelined2_16bit is
 		port(
 			X    : in  dual_rail_logic_vector(15 downto 0);
 			Y    : in  dual_rail_logic_vector(15 downto 0);
@@ -80,11 +80,11 @@ signal koxr: std_logic_vector(14 downto 0);
 signal output: dual_rail_logic_vector(15 downto 0);
 
 begin
-	ADDER15: Carry_Select_16bm
+	ADDER15: RCA_Pipelined2_16bit
 	port map(A(15), B(15), ki, sleepax(15), rst, sleepout, koa(15), output);
 	
 	GenAdder: for i in 14 downto 1 generate
-	ADDER: Carry_Select_16bm
+	ADDER: RCA_Pipelined2_16bit
 	port map(A(i), B(i), koa(i + 1), sleepax(i), rst, sleepa(i+1), koa(i), A(i+1));
 	end generate GenAdder;
 	
